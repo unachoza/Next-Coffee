af950adfabd04833263008fae3f80b3fa9e1cf09
arianna choza <unachoza@gmail.com>
2024-04-23 17:49:07 -0400

    NEXT set up files

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-blue-500 to-blue-900">
+			<Image
+				className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+				src="/next.svg"
+				alt="Next.js Logo"
+				width={180}
+				height={37}
+				priority
+			/>
+			<h1>Coffee Drinker Extraordinaire</h1>
+		</main>
+	);
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				gradient: "url('/static/background.png')",
+			},
+			colors: {
+				purple: {
+					951: "rgba(79, 70, 229, 1)",
+				},
+				gray: {
+					950: "hsla(0, 0%, 100%, 0.7);",
+					951: "hsla(0, 0%, 100%, 0.4);",
+					952: "#373b64",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
